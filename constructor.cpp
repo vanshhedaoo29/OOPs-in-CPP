0b3b90bd0bad1c70249a0edf817ce669dded850d
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-05 06:31:16 +0530

    Constructor Code Updated

--- constructor.cpp
+++ constructor.cpp
@@ -19,9 +19,32 @@ class student
         roll_no = r;
         name = n;
     }
+
+    student(const student &S)     // Copy Constructor 
+    {
+        cout << "\nCopy Constructor Invoked";
+        roll_no = S.roll_no;
+        name = S.name;
+        
+    }
+
+    void display()
+    {
+        cout << "\nRoll No. : " << roll_no << endl;
+        cout << "Name : " << name ;
+    }
 };
 
 int main()
 {
-    
+    student s1;
+    cout << "Default Constructor Invoked";
+    s1.display();
+
+    student s2(136, "Vansh");
+    cout << "\nParameterised Constructor Invoked";
+    s2.display();
+
+    student s3(s2);
+    s3.display();
 }
