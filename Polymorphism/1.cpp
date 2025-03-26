1d36be80b7cc44c8ef2ad760e3a178bd1652f6e7
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-03-26 18:05:20 +0530

    Static Function Overloading Ex 1

---
+++ Polymorphism/1.cpp
@@ -0,0 +1,45 @@
+// Static / Compile time Polymorphism
+
+// Program for Static Function Overloading
+
+#include <iostream>
+using namespace std;
+
+// Function overloading
+void display();
+void display(int);
+void display(float);
+void display(int, float);
+
+int main()
+{
+    // Correct function calls
+    display();
+    display(10);
+    display(15.5f);  // Use 'f' to explicitly declare float
+    display(20, 30.5f);
+
+    return 0;
+}
+
+// Function definitions
+void display()
+{
+    cout << "a = " << 100;
+}
+
+void display(int t)
+{
+    cout << "\nb = " << t;
+}
+
+void display(float s)
+{
+    cout << "\nc = " << s;
+}
+
+void display(int u, float v)
+{
+    cout << "\nd = " << u;
+    cout << "\ne = " << v;
+}
