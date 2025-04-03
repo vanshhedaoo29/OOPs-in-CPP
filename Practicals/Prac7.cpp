399da1ae3f5bc7ff347dfd8f649b58be891b72dd
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-04-03 14:25:07 +0530

    Practical 7 Final

--- Practicals/Prac7.cpp
+++ Practicals/Prac7.cpp
@@ -3,43 +3,46 @@ using namespace std;
 
 class Student
 {
-    private:
+private:
     int roll_no;
     string name;
 
-    public:
+public:
     void getdata()
     {
         cout << "Enter Name : ";
-        cin >> name;
+        cin.ignore();
+        getline(cin, name);
         cout << "Enter Roll no. : ";
         cin >> roll_no;
     }
 
     void setdata()
     {
-        cout << "Name : " << name ;
+        cout << "Name : " << name;
         cout << "\nRoll No. : " << roll_no << endl;
     }
 };
 
 int main()
 {
-    int n , i;
+    int n, i;
     Student s[10];
 
     cout << "Enter the number of students : ";
     cin >> n;
 
-    for(i=0 ; i<n ; i++)
+    for (i = 0; i < n; i++)
     {
-        cout << "Enter data of student " << i+1 << " :\n";
+        cout << "Enter data of student " << i + 1 << " :\n";
         s[i].getdata();
     }
 
-    for(i=0 ; i<n ; i++)
+    cout << "Student Details : \n";
+
+    for (i = 0; i < n; i++)
     {
-        cout << "Data of student " << i+1 << " :\n";
+        cout << "Data of student " << i + 1 << " :\n";
         s[i].setdata();
     }
 }
