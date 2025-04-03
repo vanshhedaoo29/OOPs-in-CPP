8d034c1c028e3250e8db30d93c94a085412751b8
Vansh Hedaoo <vanshhedaooprpote@gmail.com>
2025-04-03 14:39:48 +0530

    Practical 8 Completed

---
+++ Practicals/Prac8.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+    static int count;
+    int id;
+
+    public:
+    Counter()
+    {
+        count++;
+        id = count;
+        cout << "\nObject " << id << " Created";
+    }
+
+    ~Counter()
+    {
+        cout << "\nObject " << id << " Destroyed";
+    }
+
+    static int showcount()
+    {
+        return count;
+    }
+};
+
+int Counter :: count = 0;
+
+int main()
+{
+    Counter c1,c2,c3;
+    cout << "\n\nTotal Objects = " << Counter :: showcount() << endl;
+    return 0;
+}
+
+/*
+Output:
+Object 1 Created
+Object 2 Created
+Object 3 Created
+
+Total Objects = 3
+
+Object 3 Destroyed
+Object 2 Destroyed
+Object 1 Destroyed
+*/
